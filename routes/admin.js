78daefac02d051c77e3237d40c66650782417c5f
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2024-03-30 15:15:25 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,81 +1,106 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
-const { Admin, User, Course } = require("../db");
-const {JWT_SECRET} = require("../config");
+const { Admin, Course } = require("../db");
 const router = Router();
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Admin Routes
-router.post('/signup', async (req, res) => {
-    // Implement admin signup logic
-    const username = req.body.username;
-    const password = req.body.password;
-
-    // check if a user with this username already exists
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send({ message: "All fields are required!" });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
     await Admin.create({
-        username: username,
-        password: password
-    })
-
-    res.json({
-        message: 'Admin created successfully'
-    })
+      username: username,
+      password: hashedPassword,
+    });
+    return res.status(201).send({
+      message: "Admin created successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error creating admin account", error: error });
+  }
 });
 
-router.post('/signin', async (req, res) => {
-    // Implement admin signup logic
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log(JWT_SECRET);
-
-    const user = await User.find({
-        username,
-        password
-    })
-    if (user) {
-        const token = jwt.sign({
-            username
-        }, JWT_SECRET);
-
-        res.json({
-            token
-        })
-    } else {
-        res.status(411).json({
-            message: "Incorrect email and pass"
-        })
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send({ message: "All fields are required!" });
+  }
+  try {
+    const existingUser = await Admin.findOne({ username: username });
+    if (!existingUser) {
+      return res.status(400).send({ message: "Admin user not found" });
     }
+    const isMatched = await bcrypt.compare(password, existingUser.password);
+    if (!isMatched) {
+      return res.status(411).send({ message: "Wrong Credentials!" });
+    }
+    const token = jwt.sign(
+      { _id: existingUser._id, username: username },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+    return res.status(200).send({ token: token });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Some error occured", error: error });
+  }
 });
 
-
-router.post('/courses', adminMiddleware, async (req, res) => {
-    // Implement course creation logic
-    const title = req.body.title;
-    const description = req.body.description;
-    const imageLink = req.body.imageLink;
-    const price = req.body.price;
-    // zod
-    const newCourse = await Course.create({
-        title,
-        description,
-        imageLink,
-        price
-    })
-
-    res.json({
-        message: 'Course created successfully', courseId: newCourse._id
-    })
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const { title, description, price, imageLink } = req.body;
+  if (!title || !description || !price || !imageLink) {
+    return res.status(400).send({ message: "All fields are required!" });
+  }
+  try {
+    const adminId = req._id;
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return res.status(404).send({ message: "Admin not found" });
+    }
+    const course = await Course.create({
+      title: title,
+      description: description,
+      price: price,
+      imageLink: imageLink,
+      published: true,
+      owner: admin.username,
+    });
+    admin.courses.push(course._id);
+    await admin.save();
+    return res.status(201).send({
+      message: "Course created successfully",
+      courseId: course._id,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error creating course", error: error });
+  }
 });
 
-router.get('/courses', adminMiddleware, async (req, res) => {
-    // Implement fetching all courses logic
-    const response = await Course.find({});
-
-    res.json({
-        courses: response
-    })
-
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  try {
+    const admin = await Admin.findById(req._id).populate("courses");
+    return res.status(200).send({ courses: admin.courses });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error fetching courses", error: error });
+  }
 });
-
 
 module.exports = router;
